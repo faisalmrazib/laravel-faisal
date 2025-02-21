82851e983e068c216f99c6d8c4860cbb6b77b404
faisalmrazib <mikejimmythegoat@gmail.com>
2025-02-20 18:39:51 -0700

    bootstrap

---
+++ database/migrations/2025_02_10_021212_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+   // File: create_products_table.php
+public function up()
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id(); // Ini akan membuat kolom `id` dengan tipe BIGINT dan PRIMARY KEY
+        $table->string('name');
+        $table->decimal('price', 10, 2);
+        $table->text('description')->nullable();
+        $table->timestamps();
+    });
+
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
